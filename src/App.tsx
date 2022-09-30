717bf79e4587370485e18b755550a86cd49d3d6b
gabizzzs <glopes.p09@gmail.com>
2022-09-30 13:17:22 -0300

    adicionando react router, pagina login e o css

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-   <>
-     <Navbar />
-     <Home />
-     <Footer /> 
-   </>
+      <BrowserRouter>
+        <Navbar />
+          <div style={{ minHeight: '100vh' }}>
+            <Routes> 
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+        <Footer />
+      </BrowserRouter>
   );
 }
 
